501d12c7b78fbd788f7d777347addfc779286938
Alex Nickerson <nickersonaj@gmail.com>
2025-05-12 13:28:46 +0000

    removed useless comments

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-/*const audio = document.getElementById("audio");
-const playButton = document.getElementById("play-button");
-const playIcon = document.getElementById("play-icon");
-const rewindButton = document.getElementById("rewind-button");
-const fastforwardButton = document.getElementById("fastforward-button");
-const volumeSlider = document.getElementById("volume-slider");
-const beatDuration = document.getElementById("beat-duration");*/
-
 function formatTime(seconds) { 
     const mins = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60);
@@ -100,38 +92,3 @@ function addBeat(beat) {
 }
 
 beats.forEach(beat => addBeat(beat));
-
-/*audio.addEventListener("loadedmetadata", () => {
-  beatDuration.textContent = `0:00/${formatTime(audio.duration)}`;
-});
-audio.addEventListener("timeupdate", () => {
-    beatDuration.textContent = `${formatTime(audio.currentTime)}/${formatTime(audio.duration)}`;
-    if (audio.currentTime == audio.duration) {
-        audio.pause()
-        playIcon.src = "images/play-button.png";
-        audio.currentTime = 0;
-    }
-})
-
-function PlayPause() {
-    if (audio.paused) {
-        audio.play();
-        playIcon.src = "images/pause-button.png";
-    }
-    else {
-        audio.pause()
-        playIcon.src = "images/play-button.png";
-    }
-}
-
-function Rewind() {
-    audio.currentTime = Math.max(0, audio.currentTime - 5);
-}
-
-function FastForward() {
-    audio.currentTime = Math.min(audio.duration, audio.currentTime + 5);
-}
-
-function Volume() {
-    audio.volume = volumeSlider.value;
-}*/
